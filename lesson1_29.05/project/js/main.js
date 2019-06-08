8b56d74ae0c122af860ce0497e0285e8eb82663e
maximus.makarov@mail.ru <4a1AbdPkmh>
2019-06-08 20:04:55 +0300

    -lesson03     getRequest

--- lesson1_29.05/project/js/main.js
+++ lesson1_29.05/project/js/main.js
@@ -18,47 +18,38 @@ let getRequest = object => {
     });
 };
 
-
-// Promise
-
-// let num = a => {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             if(a){
-//                 const b = a + 10;
-//                 resolve(b)
-//             } else {
-//                 reject('Error!')
-//             }
-//
-//         }, 300)
-//     })
-// }
-
-
 class ProductsList {
     constructor(container = '.products'){
         this.container = container;
         this.data = [];
         this.allProducts = [];
-        this.init();
+        this._getProducts()
+            .then(() => this._render());
 
     }
 
-    init(){
-        this._fetchProducts();
-        this._render();
-        this._totalAmount();
-    }
+    // init(){
+    //     // this._fetchProducts();
+    //     this._render();
+    //     this._totalAmount();
+    // }
 
-    _fetchProducts(){
-        this.data = [
-            {id: 1, title: 'Notebook', price: 2000},
-            {id: 2, title: 'Mouse', price: 30},
-            {id: 3, title: 'Keyboard', price: 55},
-            {id: 4, title: 'Gamepad', price: 75}
-        ];
+    _getProducts(){
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .then(data =>{
+                this.data = [...data];
+            })
+            .catch(error => console.log('Error'));
     }
+    // _fetchProducts(){
+    //     this.data = [
+    //         {id: 1, title: 'Notebook', price: 2000},
+    //         {id: 2, title: 'Mouse', price: 30},
+    //         {id: 3, title: 'Keyboard', price: 55},
+    //         {id: 4, title: 'Gamepad', price: 75}
+    //     ];
+    // }
 
     _render(){
         const block = document.querySelector(this.container);
@@ -89,8 +80,8 @@ class ProductsList {
 
 class ProductItem {
     constructor (product, img = `https://placehold.it/200x150`){
-        this.id = product.id;
-        this.title = product.title;
+        this.id_product = product.id_product;
+        this.product_name = product.product_name;
         this.price = product.price;
         this.img = img;
     }
@@ -98,9 +89,9 @@ class ProductItem {
         return `
         <div class="d-inline-flex p-2 bd-highlight">
             <div class="card">
-                <img src="${this.img}" alt="${this.title}" title="${this.title}">
+                <img src="${this.img}" alt="${this.product_name}" title="${this.product_name}">
                 <div class="card-body">
-                    <h3 class="card-title">${this.title}</h3>
+                    <h3 class="card-title">${this.product_name}</h3>
                     <p class="card-text">${this.price}</p>
                     <a href="#" class="btn btn-primary">Купить</a>
                 </div>
